faa668c1b87b249e41849b2ef754c7fe1a6d851d
JeanMarc-RAJAONARIVELONA <jeanmarc.rajaonarivelona01@gmail.com>
2024-06-03 19:07:06 +0300

    feat: create authprovider first version

--- src/components/adminApp/index.tsx
+++ src/components/adminApp/index.tsx
@@ -2,9 +2,17 @@
 import { Admin, Resource, ListGuesser } from "react-admin";
 import MyLayout from "../layout/MyLayout";
 import { dataProvider } from "@/providers/data-providers";
+import authProviders from "@/providers/auth-providers";
+import LoginPage from "@/app/login/login";
 
 const AdminApp = () => (
-  <Admin dataProvider={dataProvider} layout={MyLayout}>
+  <Admin
+    dataProvider={dataProvider}
+    layout={MyLayout}
+    authProvider={authProviders}
+    loginPage={LoginPage}
+    requireAuth
+  >
     <Resource name="home" list={<p>hello from home</p>} />
     <Resource name="client" list={<p>not implemented</p>} />
     <Resource name="car" list={ListGuesser} />
