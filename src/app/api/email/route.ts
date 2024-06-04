4b39a220627dfe31b3adb6624e85d03b083c42e1
Dinoh04 <dino.rainimarolahy@gmail.com>
2024-06-04 23:24:33 +0300

    Feat: Feat: send email in server side

--- src/app/api/email/route.ts
+++ src/app/api/email/route.ts
@@ -1,7 +1,7 @@
 import Welcome from "@/app/emails/Welcome";
 import { Resend } from "resend";
 
-const resend = new Resend(process.env.RESEND_API_KEY);
+const resend = new Resend('re_UnHFNcua_Je5fowNwR62fBsPgQH3vPWqb');
 
 export async function POST(req: Request) {
   const { email, firstname } = await req.json();
